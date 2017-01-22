41a3ea0d87671aac63fc91b49a63cbb4bce2d0e4
minnesberg <minnesberg@gmail.com>
2017-01-22 22:24:39 +0100

    Add files via upload

---
+++ DistansFungerande.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+unsigned long SLEEP_TIME = 5000; // Sleep time between reads (in milliseconds)
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+int lastDist;
+bool metric = true;
+
+void setup()  
+{
+Serial.begin(9600);
+}
+
+void loop()      
+{     
+  int dist = metric?sonar.ping_cm():sonar.ping_in();
+  Serial.print("Distans: ");
+  Serial.print(dist); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  Serial.println(" cm");
+
+
+  delay(2000);
+}
